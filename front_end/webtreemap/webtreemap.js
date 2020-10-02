b07146ece303d545550283e76d6f0cb82812caf9
Paul Irish <paulirish@chromium.org>
2020-10-01 18:56:42 -0700

    add more files

---
+++ front_end/webtreemap/webtreemap.js
@@ -0,0 +1,10 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+// import '../third_party/codemirror/package/addon/fold/brace-fold.js';
+
+import WebTreeMap from '../third_party/webtreemap/webtreemap.js';
+
+export {WebTreeMap};
