5f109808aa3479e3919ed4a61765dad8adf1e65c
Paul Irish <paulirish@chromium.org>
2023-03-21 18:46:21 -0700

    traceargs highlighting

--- front_end/panels/timeline/TimelineUIUtils.ts
+++ front_end/panels/timeline/TimelineUIUtils.ts
@@ -42,6 +42,7 @@ import * as TimelineModel from '../../models/timeline_model/timeline_model.js';
 import * as TraceEngine from '../../models/trace/trace.js';
 import * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
 import * as Components from '../../ui/legacy/components/utils/utils.js';
+import * as CodeHighlighter from '../../ui/components/code_highlighter/code_highlighter.js';
 import * as UI from '../../ui/legacy/legacy.js';
 
 import type * as Protocol from '../../generated/protocol.js';
@@ -1708,12 +1709,6 @@ export class TimelineUIUtils {
           detailsText = null;
         } else {
           details = linkifyTopCallFrame();
-          if (!Object.keys(event.args).length) return details;
-
-          details = details ?? document.createElement('span');
-          const omg = document.createElement('tt');
-          omg.textContent = Object.keys(event.args).length ? JSON.stringify(event.args, null, 2) : '';
-          details.append(omg);
         }
         break;
       }
@@ -2253,6 +2248,26 @@ export class TimelineUIUtils {
         TimelineModel.TimelineModel.InvalidationTracker.invalidationEventsFor(event)) {
       TimelineUIUtils.generateCauses(event, model.targetByEvent(event), relatedNodesMap, contentHelper);
     }
+
+    // trace event args.
+    contentHelper.addSection('Trace event args');
+    const argsContainer = document.createElement('div');
+    const shadowRoot = argsContainer.attachShadow({mode: 'open'});
+    shadowRoot.adoptedStyleSheets = [CodeHighlighter.Style.default];
+    const argsEl = shadowRoot.createChild('div') as HTMLDivElement;
+    argsEl.classList.add('traceargs');
+    // I cant figure out styles and shadow dom. nastyhack.
+    argsEl.style.cssText = `
+      font-family: var(--monospace-font-family);
+      font-size: var(--monospace-font-size) !important;
+      white-space: pre-wrap;
+      line-height: 1;
+      display: inline-block;
+    `
+    const argsTxt = Object.keys(event.args).length ? JSON.stringify(event.args, null, 2) : '';
+    argsEl.textContent = argsTxt.replace(/{\n  /, '{ ');
+    void CodeHighlighter.CodeHighlighter.highlightNode(argsEl, 'text/javascript');
+    contentHelper.appendElementRow('', argsContainer);
 
     const stats: {
       [x: string]: number,
