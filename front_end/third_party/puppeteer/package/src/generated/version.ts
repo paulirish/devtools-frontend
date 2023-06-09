38770ec6fc2899cb8938372072b10074e05b23de
Paul Irish <paulirish@chromium.org>
2023-06-09 13:33:17 -0700

    ts soruces

---
+++ front_end/third_party/puppeteer/package/src/generated/version.ts
@@ -0,0 +1,4 @@
+/**
+ * @internal
+ */
+export const packageVersion = '19.2.1';
---
+++ front_end/third_party/puppeteer/package/src/node/ProductLauncher.ts
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {existsSync} from 'fs';
+import os, {tmpdir} from 'os';
+import {join} from 'path';
+import {Browser} from '../api/Browser.js';
+import {Product} from '../common/Product.js';
+import {BrowserFetcher} from './BrowserFetcher.js';
+import {
+  BrowserLaunchArgumentOptions,
+  ChromeReleaseChannel,
+  PuppeteerNodeLaunchOptions,
+} from './LaunchOptions.js';
+import {PuppeteerNode} from './PuppeteerNode.js';
+
+/**
+ * Describes a launcher - a class that is able to create and launch a browser instance.
+ *
+ * @public
+ */
+export class ProductLauncher {
+  #product: Product;
+
+  /**
+   * @internal
+   */
+  puppeteer: PuppeteerNode;
+
+  /**
+   * @internal
+   */
+  constructor(puppeteer: PuppeteerNode, product: Product) {
+    this.puppeteer = puppeteer;
+    this.#product = product;
+  }
+
+  get product(): Product {
+    return this.#product;
+  }
+
+  launch(object: PuppeteerNodeLaunchOptions): Promise<Browser>;
+  launch(): Promise<Browser> {
+    throw new Error('Not implemented');
+  }
+
+  executablePath(channel?: ChromeReleaseChannel): string;
+  executablePath(): string {
+    throw new Error('Not implemented');
+  }
+
+  defaultArgs(object: BrowserLaunchArgumentOptions): string[];
+  defaultArgs(): string[] {
+    throw new Error('Not implemented');
+  }
+
+  /**
+   * @internal
+   */
+  protected getProfilePath(): string {
+    return join(
+      this.puppeteer.configuration.temporaryDirectory ?? tmpdir(),
+      `puppeteer_dev_${this.product}_profile-`
+    );
+  }
+
+  /**
+   * @internal
+   */
+  protected resolveExecutablePath(): string {
+    const executablePath = this.puppeteer.configuration.executablePath;
+    if (executablePath) {
+      if (!existsSync(executablePath)) {
+        throw new Error(
+          `Tried to find the browser at the configured path (${executablePath}), but no executable was found.`
+        );
+      }
+      return executablePath;
+    }
+
+    const ubuntuChromiumPath = '/usr/bin/chromium-browser';
+    if (
+      this.product === 'chrome' &&
+      os.platform() !== 'darwin' &&
+      os.arch() === 'arm64' &&
+      existsSync(ubuntuChromiumPath)
+    ) {
+      return ubuntuChromiumPath;
+    }
+
+    const browserFetcher = new BrowserFetcher({
+      product: this.product,
+      path: this.puppeteer.defaultDownloadPath!,
+    });
+
+    const revisionInfo = browserFetcher.revisionInfo(
+      this.puppeteer.browserRevision
+    );
+    if (!revisionInfo.local) {
+      if (this.puppeteer.configuration.browserRevision) {
+        throw new Error(
+          `Tried to find the browser at the configured path (${revisionInfo.executablePath}) for revision ${this.puppeteer.browserRevision}, but no executable was found.`
+        );
+      }
+      switch (this.product) {
+        case 'chrome':
+          throw new Error(
+            `Run \`npm install\` to download the correct Chromium revision (${this.puppeteer.browserRevision}).`
+          );
+        case 'firefox':
+          throw new Error(
+            `Run \`PUPPETEER_PRODUCT=firefox npm install\` to download a supported Firefox browser binary.`
+          );
+      }
+    }
+    return revisionInfo.executablePath;
+  }
+}
