6201d2251e11d35d876af8ef907546a49b1cc5e1
Paul Irish <paulirish@chromium.org>
2023-07-26 14:56:55 -0700

    add thousands separators to console numbers

--- front_end/panels/console/ConsoleViewMessage.ts
+++ front_end/panels/console/ConsoleViewMessage.ts
@@ -191,6 +191,8 @@ const UIStrings = {
 const str_ = i18n.i18n.registerUIStrings('panels/console/ConsoleViewMessage.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 const elementToMessage = new WeakMap<Element, ConsoleViewMessage>();
+const formatter = Intl.NumberFormat(undefined, { maximumFractionDigits: 20});
+const log10 = Math.log(10);
 
 export const getMessageForElement = (element: Element): ConsoleViewMessage|undefined => {
   return elementToMessage.get(element);
@@ -690,6 +692,14 @@ export class ConsoleViewMessage implements ConsoleViewportElement {
     return element;
   }
 
+  // https://stackoverflow.com/a/22885197
+  private getSignificantDigitCount(numberStr: string):number {
+    let n = Math.abs(parseInt(numberStr.replace('.', ''), 10)); // remove decimal and make positive
+    if (n === 0) {return 0;}
+    while (n !== 0 && n % 10 === 0) {n /= 10;} // kill the 0s at the end of n
+    return Math.floor(Math.log(n) / log10) + 1; // get number of digits
+  }
+
   private formatParameterAsValue(obj: SDK.RemoteObject.RemoteObject): HTMLElement {
     const result = document.createElement('span');
     const description = obj.description || '';
@@ -697,6 +707,15 @@ export class ConsoleViewMessage implements ConsoleViewportElement {
       const propertyValue = new ObjectUI.ObjectPropertiesSection.ExpandableTextPropertyValue(
           document.createElement('span'), description, getLongStringVisibleLength());
       result.appendChild(propertyValue.element);
+    } else
+     // thousands separators:
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#parameters:~:text=1%20to%2021%3B-,the%20default%20is%2021,-.
+      // 17 for safety. not really sure tbh
+      if (obj.type === 'number' && this.getSignificantDigitCount(description) < 17) {
+          const parts = formatter.formatToParts(parseFloat(description));
+          parts.filter(p => p.type === 'group').forEach(p => { p.value = '_';});
+          const reformatted = parts.map(p => p.value).join('');
+          UI.UIUtils.createTextChildren(result, reformatted);
     } else {
       UI.UIUtils.createTextChild(result, description);
     }
