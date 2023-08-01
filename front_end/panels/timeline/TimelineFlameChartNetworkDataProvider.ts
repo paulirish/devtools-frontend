70dfafddd2234e2cb1a8f708db7bbbfbcdad515b
Paul Irish <paulirish@chromium.org>
2023-08-01 13:46:47 -0700

    format

--- front_end/panels/timeline/TimelineFlameChartNetworkDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartNetworkDataProvider.ts
@@ -15,8 +15,8 @@ import {NetworkTrackAppender} from './NetworkTrackAppender.js';
 import timelineFlamechartPopoverStyles from './timelineFlamechartPopover.css.js';
 import {FlameChartStyle, Selection} from './TimelineFlameChartView.js';
 import {TimelineSelection} from './TimelinePanel.js';
-import {TimelineUIUtils} from './TimelineUIUtils.js';
 import {TimelineSelection} from './TimelineSelection.js';
+import {TimelineUIUtils} from './TimelineUIUtils.js';
 
 export class TimelineFlameChartNetworkDataProvider implements PerfUI.FlameChart.FlameChartDataProvider {
   #minimumBoundaryInternal: number;
@@ -262,26 +262,31 @@ export class TimelineFlameChartNetworkDataProvider implements PerfUI.FlameChart.
     const request = event;
     const darkened = this.entryColor(index).replace(/\d+%\)/, '20%, 0.4)').replace('hsl', 'hsla');
     context.fillStyle = darkened;
-    const receiveDataEvents = [request.args.data.receiveResponse, ...request.args.data.receiveDatas ?? []]; // everything with encodedDataLength (thats not resourcefinish)
-    const totalDataChunkLength = receiveDataEvents.reduce((sum, item) => sum + item.args?.data?.encodedDataLength || 0, 0);
+    const receiveDataEvents = [
+      request.args.data.receiveResponse,
+      ...request.args.data.receiveDatas ?? [],
+    ];  // everything with encodedDataLength (thats not resourcefinish)
+    const totalDataChunkLength =
+        receiveDataEvents.reduce((sum, item) => sum + item.args?.data?.encodedDataLength || 0, 0);
     // each "data received" event in blink gets a 'length' from a chunk.size().
     // This number probaly includes some overhead (mojo IPC chunk metadata?) and generally likes to be 65k.
     //  so bytesSum will generally be larger than the request's encodedDataLength. Shrug.
     const adjFactor = totalDataChunkLength / request.args.data.encodedDataLength;
 
     const beginTime = TraceEngine.Helpers.Timing.microSecondsToMilliseconds(event.ts);
-    const timeToPixel = (time: number): number => Math.floor(unclippedBarX + (TraceEngine.Helpers.Timing.microSecondsToMilliseconds(time) - beginTime) * timeToPixelRatio);
+    const timeToPixel = (time: number): number => Math.floor(
+        unclippedBarX + (TraceEngine.Helpers.Timing.microSecondsToMilliseconds(time) - beginTime) * timeToPixelRatio);
 
     let bytesSum = receiveDataEvents[0].args.data.encodedDataLength || 0;
     for (let i = 1; i < receiveDataEvents.length; i++) {
-        const prevEvt = receiveDataEvents[i-1];
-        const evt = receiveDataEvents[i];
-        const chunkLength = evt.args.data.encodedDataLength;
-        bytesSum += chunkLength;
-        const scaledHeight = Math.min(bytesSum / request.args.data.encodedDataLength / adjFactor, 100) * (barHeight - 1);
-        // console.log(evt.name, bytesSum / request.encodedDataLength / adjFactor, 'here', chunkLength, 'totalling', bytesSum, 'out of', request.encodedDataLength, ' umwhat', scaledHeight);
-        const stepRectWidth = timeToPixel(evt.ts) - timeToPixel(prevEvt.ts);
-        context.fillRect(timeToPixel(prevEvt.ts), barY, stepRectWidth, scaledHeight);
+      const prevEvt = receiveDataEvents[i - 1];
+      const evt = receiveDataEvents[i];
+      const chunkLength = evt.args.data.encodedDataLength;
+      bytesSum += chunkLength;
+      const scaledHeight = Math.min(bytesSum / request.args.data.encodedDataLength / adjFactor, 100) * (barHeight - 1);
+      // console.log(evt.name, bytesSum / request.encodedDataLength / adjFactor, 'here', chunkLength, 'totalling', bytesSum, 'out of', request.encodedDataLength, ' umwhat', scaledHeight);
+      const stepRectWidth = timeToPixel(evt.ts) - timeToPixel(prevEvt.ts);
+      context.fillRect(timeToPixel(prevEvt.ts), barY, stepRectWidth, scaledHeight);
     }
 
     return true;
