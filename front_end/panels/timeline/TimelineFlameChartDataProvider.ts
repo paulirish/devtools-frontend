3a3b778c0a9b9fddf70ec0624be365b624aecf34
Paul Irish <paulirish@chromium.org>
2023-08-11 17:04:58 -0700

    threadpoolworkers into a group thing - good trace for this: arizona-framedestroyed2.json

--- front_end/panels/timeline/TimelineFlameChartDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartDataProvider.ts
@@ -84,6 +84,11 @@ const UIStrings = {
   rasterizerThreadS: 'Rasterizer Thread {PH1}',
   /**
    *@description Text in Timeline Flame Chart Data Provider of the Performance panel
+   *@example {2} PH1
+   */
+  threadPoolWorkerS: 'Thread pool worker {PH1}',
+  /**
+   *@description Text in Timeline Flame Chart Data Provider of the Performance panel
    */
   thread: 'Thread',
   /**
@@ -149,6 +154,12 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
    * rendering a track for thread.
    */
   #rasterCount: number = 0;
+  /**
+   * Thread pool threads are tracked and enumerated with this property. This is also
+   * used to group all threadpool threads together in the same track, instead of
+   * rendering a track for thread.
+   */
+  #threadPoolCount: number = 0;
 
   private minimumBoundaryInternal: number;
   private timeSpan: number;
@@ -369,6 +380,8 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
     this.compatibilityTracksAppender = null;
     this.#eventToDisallowRoot = new WeakMap<TraceEngine.Legacy.Event, boolean>();
     this.#indexForEvent = new WeakMap<TraceEngine.Legacy.Event, number>();
+    this.#rasterCount = 0;
+    this.#threadPoolCount = 0;
   }
 
   maxStackDepth(): number {
@@ -471,8 +484,10 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
           return 7;
         case TimelineModel.TimelineModel.TrackType.Raster:
           return 9;
-        case TimelineModel.TimelineModel.TrackType.Other:
+        case TimelineModel.TimelineModel.TrackType.ThreadPool:
           return 10;
+        case TimelineModel.TimelineModel.TrackType.Other:
+          return 11;
         default:
           return -1;
       }
@@ -558,6 +573,18 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
         ++this.#rasterCount;
         this.appendSyncEvents(
             track, track.events, i18nString(UIStrings.rasterizerThreadS, {PH1: this.#rasterCount}), this.headerLevel2,
+            eventEntryType, true /* selectable */, expanded);
+        break;
+      }
+
+      case TimelineModel.TimelineModel.TrackType.ThreadPool: {
+        if (!this.#threadPoolCount) {
+          this.appendHeader('Thread Pool', this.headerLevel1, false /* selectable */, expanded);
+        }
+        ++this.#threadPoolCount;
+        this.appendSyncEvents(
+            // TODO fix thread name
+            track, track.events, i18nString(UIStrings.threadPoolWorkerS, {PH1: this.#threadPoolCount}), this.headerLevel2,
             eventEntryType, true /* selectable */, expanded);
         break;
       }
