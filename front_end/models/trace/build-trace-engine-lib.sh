c92d28566bf4956cc5dc09bf704bfc75711b297a
Paul Irish <paulirish@chromium.org>
2023-08-29 10:31:41 -0700

    got a module

--- front_end/models/trace/build-trace-engine-lib.sh
+++ front_end/models/trace/build-trace-engine-lib.sh
@@ -1,9 +1,24 @@
+#!/usr/bin/env bash
 
-~/chromium-devtools/devtools-frontend/third_party/esbuild/esbuild \
-      --outdir=/Users/paulirish/chromium-devtools/devtools-frontend/out/Default/gen/cooltrace \
+set -euxo pipefail
+
+# usage:
+#   watchexec "front_end/models/trace/build-trace-engine-lib.sh && node analyze-trace.mjs"
+
+dtfe="/Users/paulirish/chromium-devtools/devtools-frontend"
+
+$dtfe/third_party/esbuild/esbuild \
+      --outdir=$dtfe/out/Default/gen/cooltrace \
+      --out-extension:.js=.mjs \
       --log-level=info \
       --sourcemap  \
       --bundle --tree-shaking=true \
-      --metafile=/Users/paulirish/chromium-devtools/devtools-frontend/out/Default/gen/cooltrace/meta.json \
+      --format=esm \
+      --metafile=$dtfe/out/Default/gen/cooltrace/meta.json \
       --external:"*TracingManager.js" --external:"*extras.js" \
-      ~/chromium-devtools/devtools-frontend/front_end/models/trace/trace.ts
+      $dtfe/front_end/models/trace/trace.ts
+
+
+touch $dtfe/out/Default/gen/cooltrace/TracingManager.js
+mkdir -p $dtfe/out/Default/gen/cooltrace/extras/
+touch $dtfe/out/Default/gen/cooltrace/extras/extras.js
