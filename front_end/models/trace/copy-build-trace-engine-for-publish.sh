2a36047304b4bb99a2166d1154270dbe867035e9
Paul Irish <paulirish@chromium.org>
2023-08-29 13:24:24 -0700

    cool

--- front_end/models/trace/copy-build-trace-engine-for-publish.sh
+++ front_end/models/trace/copy-build-trace-engine-for-publish.sh
@@ -20,7 +20,7 @@ cp -rp "$dtfe/test/unittests/fixtures/traces/invalid-animation-events.json.gz" "
 # tweak it for the new location
 cp -rp "$dtfe/analyze-trace.mjs" "$standalone/analyze-trace.mjs.orig"
 cat "$standalone/analyze-trace.mjs.orig"        | \
-  sed 's|out/Default/gen/trace_engine/|./|'     | \
+  sed 's|./out/Default/gen/trace_engine/|./|'     | \
   sed 's|test/unittests/fixtures/traces/|test/|'  > "$standalone/analyze-trace.mjs"
 
 command rm "$standalone/analyze-trace.mjs.orig"
