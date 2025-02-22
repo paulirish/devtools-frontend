29b85e3ad6c767e99a93522da9136ca8c92aefd4
Paul Irish <paulirish@chromium.org>
2025-02-21 18:02:13 -0800

    match on entity identity not name

--- front_end/panels/timeline/ThirdPartyTreeView.ts
+++ front_end/panels/timeline/ThirdPartyTreeView.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as Trace from '../../models/trace/trace.js';
 import * as DataGrid from '../../ui/legacy/components/data_grid/data_grid.js';
