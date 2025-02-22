b527efce5f331693a800b1716edc89aac1cb413d
Paul Irish <paulirish@chromium.org>
2025-02-22 15:50:47 -0800

    this failing test

--- front_end/panels/timeline/utils/EntityMapper.test.ts
+++ front_end/panels/timeline/utils/EntityMapper.test.ts
@@ -12,8 +12,8 @@ describeWithEnvironment.only('EntityMapper', function() {
   it('correctly merges handler data', async function() {
     const {parsedTrace} = await TraceLoader.traceEngine(this, 'lantern/paul/trace.json.gz');
 
-    const fromRenderer = new Map(parsedTrace.Renderer.entityMappings.eventsByEntity);
-    const fromNetwork = new Map(parsedTrace.NetworkRequests.entityMappings.eventsByEntity);
+    const fromRenderer = parsedTrace.Renderer.entityMappings.eventsByEntity;
+    const fromNetwork = parsedTrace.NetworkRequests.entityMappings.eventsByEntity;
 
     const mapper = new Utils.EntityMapper.EntityMapper(parsedTrace);
     const mappings = mapper.mappings();
@@ -39,12 +39,9 @@ describeWithEnvironment.only('EntityMapper', function() {
       });
     });
 
-    // Additionally make sure they sum up.
-    mappings.eventsByEntity.entries().forEach(([entity, events]) => {
-      const eventsInNetwork = fromNetwork.get(entity) ?? [];
-      const eventsInRenderer = fromRenderer.get(entity) ?? [];
-      assert.deepEqual(events.length, eventsInNetwork.length + eventsInRenderer.length);
-    });
+    // These would be the same object identity, if not for shallowClone
+    assert.deepEqual(fromRenderer, fromNetwork);
+    assert.deepEqual(fromRenderer, mappings.eventsByEntity);
   });
 
   describe('entityForEvent', () => {
