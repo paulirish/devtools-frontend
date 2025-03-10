4c3e466aba7d70e8c72c22556f4d38ae2ce80ac8
Connor Clark <cjamcl@chromium.org>
2025-03-10 16:01:19 -0700

    add Iterator.prototype.flatMap polyfill

--- scripts/trace/prep-trace-engine-package.sh
+++ scripts/trace/prep-trace-engine-package.sh
@@ -86,6 +86,9 @@ for p in Path('$dist/models').rglob('*.d.ts'):
     needle = 'CrUXManager.Scope'
     content = content.replace(needle, 'any')
 
+    needle = 'anyV3'
+    content = content.replace(needle, 'any')
+
     p.write_text(content)
 "
 
